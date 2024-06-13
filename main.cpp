f5a3ae243eb87c63b1953d51c74beb2ce6229dfa
jinwoo <sjw6599@snu.ac.kr>
2024-06-13 15:55:57 +0000

    신진우

--- main.cpp
+++ main.cpp
@@ -5,18 +5,28 @@ using namespace std;
 
 int N,M;
 vector<int> vec;
+vector<int> cur;
+void func(int cnt, int ind) {
+if(cnt == M){
+	for (int num : current){
+		cout << num << " ";
+	}
+	cout << endl;
+	return;
+}
 
-void func(int cnt) {
-
-
-
-
-
-
+for (int i=ind; i<N; ++i){
+	current.push_back(vec[i]);
+	func(i+1,cnt+1);
+	current.pop_back();
+}
 
 }
 
 int main() {
    cin >> N >> M;
-   func(0);
+   for (z=1; z<=N; z++){
+	   vec.push_back(z);
+   }
+   func(0,0);
 }
